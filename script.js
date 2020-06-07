d85dd6ef4eb1393c4a59f5e78daec637a40a6318
Manav Kapila <you@example.com>
2020-06-07 23:08:58 +0530

    enhanced styles

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 var css = document.querySelector("h3");
+// to select elements
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var color4 = document.querySelector(".color4");
 var body = document.getElementById("gradient");
-
+// function to set gradient color
 function setGradient() {
+    'use strict';
 	body.style.background = 
-	"linear-gradient(to right, " 
+	"linear-gradient(90deg, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
+	+ ","
+    + color3.value 
+	+ ", " 
+	+ color4.value 
 	+ ")";
 
 	css.textContent = body.style.background + ";";
@@ -17,3 +25,7 @@ function setGradient() {
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient);
+
+color4.addEventListener("input", setGradient);
